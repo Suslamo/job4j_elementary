40fab01fa9f51e974584e549e01d7832fe55d153
Suslamo <aualamo@mail.ru>
2021-08-16 19:48:17 +0700

    Разбор первой программы. [#159033]

---
+++ src/ru/job4j/Studentinfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class Studentinfo {
+    public static void main (String [] agrs){
+        System.out.println("Andrey Ikonnikov");
+        System.out.println("01.05.1996");
+    }
+}
