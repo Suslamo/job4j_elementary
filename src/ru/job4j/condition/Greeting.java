c0ebd2d9829d2039b449427c62391a4cfcb7abc3
Suslamo <aualamo@mail.ru>
2021-08-25 20:27:58 +0700

    String [#237407]

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "i like Java";
+        System.out.print(idea);
+        idea = idea + "But I am a newbie.";
+        int year = 2021;
+        idea = idea + year;
+        System.out.print(idea);
+
+    }
+}
